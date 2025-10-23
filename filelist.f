3a41caed6d9a5fa668ef1c7d9301ee7f96cec62e
Peter Grove <82904134+phgrove@users.noreply.github.com>
2025-10-23 17:18:14 +0100

    Update filelist.f

--- filelist.f
+++ filelist.f
@@ -1,2 +1,13 @@
 //Command file
 +optconfigfile+ams.config.cfg 
+
+//vcsAD.init file
+-ad=vcsAD.init
+
+//AMS setup options
++verilogamsext+vams
+-xlrm ams
+-xlrm ams_worklib
+-ams
+-ams_dresolution
+-ams_iereport
